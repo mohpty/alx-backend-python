ed4c8b3e25d59a947c673c27281af1b3cfe1cfda
Mohammad Fathi <mohfabdou@gmail.com>
2024-07-05 00:48:10 +0300

    solved first two tasks

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Function type annotation"""
+
+
+def add(a: float, b: float) -> float:
+    '''Return sum of a and b'''
+    return (a + b)
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''includes floor function'''
+from math import floor
+
+
+def floor(n: float) -> int:
+    '''Returns floor of a float'''
+
+    return floor(n)
