62a5865a8b3873081df42fe948cc927d4ddc2cca
Mohammad Fathi <mohfabdou@gmail.com>
2024-07-05 01:10:50 +0300

    solved task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Includes the function sum_mixed_list
+'''
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+    '''
+    return the sum of mix of ints and floats
+    '''
+
+    return sum(mxd_lst)
