33edbe3dfce22b1517f7aad7d69cd0e37262571f
Mohammad Fathi <mohfabdou@gmail.com>
2024-07-05 01:22:29 +0300

    fixed 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 '''Includes to_kv function'''
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''
     returns a customized tuple
     '''
