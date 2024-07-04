9a402c270b7063ee2ce2f2c9776e7b1224d712a5
Mohammad Fathi <mohfabdou@gmail.com>
2024-07-05 00:54:00 +0300

    solved task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Includes to_str function'''
+
+
+def to_str(n: float) -> str:
+    '''Return the string representation
+    of the parameter n'''
+
+    return n.__str__()
