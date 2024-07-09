6865a243fc56f49ded4dd3aa2ada977314225fcd
Mohammad Fathi <mohfabdou@gmail.com>
2024-07-10 00:47:29 +0300

    init

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Async Comprehensions
+"""
+
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Collect some random numbers using async
+    """
+    return [i async for i in async_generator()]
